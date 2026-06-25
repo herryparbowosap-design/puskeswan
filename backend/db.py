075519f372e9800ac93ce3343659e8516dfa7a4c
herryparbowosap-design <herryparbowo.sap@gmail.com>
2026-06-25 13:45:53 +0700

    Create db.py

---
+++ backend/db.py
@@ -0,0 +1,16 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "puskeswan")
+
+_client = None
+
+def get_client() -> AsyncIOMotorClient:
+    global _client
+    if _client is None:
+        _client = AsyncIOMotorClient(MONGO_URL)
+    return _client
+
+def get_db():
+    return get_client()[DB_NAME]
